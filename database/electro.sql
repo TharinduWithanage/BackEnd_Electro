2c4294660dd1c191e2ddaf656c907debaf7354ac
Minuri427 <yasarawickramanayaka@gmail.com>
2021-07-13 01:25:13 +0530

    add database

---
+++ database/electro.sql
@@ -0,0 +1,361 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2021 at 09:40 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `electro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `Cust_id` int(11) NOT NULL,
+  `First_name` varchar(50) NOT NULL,
+  `Last_name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Role` varchar(20) NOT NULL DEFAULT 'customer'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`Cust_id`, `First_name`, `Last_name`, `Email`, `Password`, `Role`) VALUES
+(1, 'Minuri', 'Wickramanayaka', 'minuri@gmail.com', '$2b$10$rt9yvwkTymAohJYgl0E/ZORvAMlcx8dn48bdNjt4snHhznJcGfXYa', 'customer'),
+(2, 'Yasara', 'Wickramanayaka', 'yasara@gmail.com', '$2b$10$Wj7RiNfHy7yugJqCN5mXWez5lyo.YB8iodbgBG2gd42WX04ccjcK.', 'customer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ebill_monthly_plan`
+--
+
+CREATE TABLE `ebill_monthly_plan` (
+  `Bill_id` varchar(10) NOT NULL,
+  `Total_cost_tou` float NOT NULL,
+  `Cost_day_time` float NOT NULL,
+  `Cost_off_peak_time` float NOT NULL,
+  `Cost_peak_time` float NOT NULL,
+  `Total_cost_fixed` float NOT NULL,
+  `Total_units_tou` int(11) NOT NULL,
+  `Units_day_time` int(11) NOT NULL,
+  `Units_off_peak_time` int(11) NOT NULL,
+  `Units_peak_time` int(11) NOT NULL,
+  `Total_units_fixed` int(11) NOT NULL,
+  `Best_model` int(11) NOT NULL,
+  `Cust_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ebill_specialevent`
+--
+
+CREATE TABLE `ebill_specialevent` (
+  `Bill_id` varchar(10) NOT NULL,
+  `No_of_days` int(11) NOT NULL,
+  `Total_cost_tou` float NOT NULL,
+  `Cost_day_time` float NOT NULL,
+  `Cost_off_peak_time` float NOT NULL,
+  `Cost_peak_time` float NOT NULL,
+  `Total_units_tou` int(11) NOT NULL,
+  `Units_day_time` int(11) NOT NULL,
+  `Units_off_peak_time` int(11) NOT NULL,
+  `Units_peak_time` int(11) NOT NULL,
+  `Total_units_fixed` int(11) NOT NULL,
+  `Cust_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `electric_device_mplan`
+--
+
+CREATE TABLE `electric_device_mplan` (
+  `Device_id` varchar(10) NOT NULL,
+  `Bill_id` varchar(10) NOT NULL,
+  `Device_name` varchar(50) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Priority` varchar(20) NOT NULL,
+  `Using_hours_peak_time` float NOT NULL,
+  `Using_hours_off_peak_time` float NOT NULL,
+  `Using_hours_day_time` float NOT NULL,
+  `Power(W)` float NOT NULL,
+  `Total_units_fixed` int(11) NOT NULL,
+  `Units_peak_time` int(11) NOT NULL,
+  `Units_off_peak_time` int(11) NOT NULL,
+  `Units_day_time` int(11) NOT NULL,
+  `Total_cost_fixed` float NOT NULL,
+  `Cost_peak_time` float NOT NULL,
+  `Cost_off_peak_time` float NOT NULL,
+  `Cost_day_time` float NOT NULL,
+  `Cust_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `electric_device_special_event`
+--
+
+CREATE TABLE `electric_device_special_event` (
+  `Device_id` varchar(10) NOT NULL,
+  `Bill_id` varchar(10) NOT NULL,
+  `Device_name` varchar(50) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Priority` varchar(20) NOT NULL,
+  `Using_hours_peak_time` float NOT NULL,
+  `Using_hours_off_peak_time` float NOT NULL,
+  `Using_hours_day_time` float NOT NULL,
+  `Power(W)` float NOT NULL,
+  `Total_units_fixed` int(11) NOT NULL,
+  `Units_off_peak_time` int(11) NOT NULL,
+  `Units_peak_time` int(11) NOT NULL,
+  `Units_day_time` int(11) NOT NULL,
+  `Cost_day_time` float NOT NULL,
+  `Cost_peak_time` float NOT NULL,
+  `Cost_off_peak_time` float NOT NULL,
+  `Cust_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `Emp_id` int(10) NOT NULL,
+  `First_name` varchar(50) NOT NULL,
+  `Last_name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Conatact_no` varchar(10) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `NIC` varchar(15) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `Enrollment_date` date NOT NULL,
+  `Role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`Emp_id`, `First_name`, `Last_name`, `Email`, `Conatact_no`, `Password`, `NIC`, `Address`, `Enrollment_date`, `Role`) VALUES
+(1, 'Tharindu', 'Withanage', 'admin1@gmail.com', '0711565953', '$2b$10$rt9yvwkTymAohJYgl0E/ZORvAMlcx8dn48bdNjt4snHhznJcGfXYa', '974260018v', 'Matara', '2021-07-21', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fixed_ucharge`
+--
+
+CREATE TABLE `fixed_ucharge` (
+  `Unit_category` varchar(50) NOT NULL,
+  `Unit_charge` float NOT NULL,
+  `Fixed_charge` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `fixed_ucharge`
+--
+
+INSERT INTO `fixed_ucharge` (`Unit_category`, `Unit_charge`, `Fixed_charge`) VALUES
+('0-30', 2.5, 30),
+('0-60', 7.85, 0),
+('121-180', 32, 480),
+('31-60', 4.85, 60),
+('61-90', 10, 90),
+('91-120', 27.75, 480),
+('More than 180', 45, 540);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggestions_tou`
+--
+
+CREATE TABLE `suggestions_tou` (
+  `Suggest_id` varchar(10) NOT NULL,
+  `Device_id` varchar(10) NOT NULL,
+  `Bill_id` varchar(10) NOT NULL,
+  `Device_name` varchar(20) NOT NULL,
+  `Priority` varchar(50) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Sugesstions` varchar(500) NOT NULL,
+  `Cust_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tou_ucharge`
+--
+
+CREATE TABLE `tou_ucharge` (
+  `Time_category` varchar(50) NOT NULL,
+  `Fixed_charge` float NOT NULL,
+  `Unit_charge` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tou_ucharge`
+--
+
+INSERT INTO `tou_ucharge` (`Time_category`, `Fixed_charge`, `Unit_charge`) VALUES
+('Day', 25, 540),
+('Off Peak', 13, 540),
+('Peak', 54, 540);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Cust_id`),
+  ADD UNIQUE KEY `UK1` (`Email`);
+
+--
+-- Indexes for table `ebill_monthly_plan`
+--
+ALTER TABLE `ebill_monthly_plan`
+  ADD PRIMARY KEY (`Bill_id`),
+  ADD KEY `FK1` (`Cust_id`);
+
+--
+-- Indexes for table `ebill_specialevent`
+--
+ALTER TABLE `ebill_specialevent`
+  ADD PRIMARY KEY (`Bill_id`),
+  ADD KEY `FK2` (`Cust_id`);
+
+--
+-- Indexes for table `electric_device_mplan`
+--
+ALTER TABLE `electric_device_mplan`
+  ADD PRIMARY KEY (`Device_id`,`Bill_id`),
+  ADD KEY `FK3` (`Bill_id`),
+  ADD KEY `FK4` (`Cust_id`);
+
+--
+-- Indexes for table `electric_device_special_event`
+--
+ALTER TABLE `electric_device_special_event`
+  ADD PRIMARY KEY (`Device_id`,`Bill_id`) USING BTREE,
+  ADD KEY `FK7` (`Bill_id`),
+  ADD KEY `FK8` (`Cust_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`Emp_id`),
+  ADD UNIQUE KEY `UK2` (`Email`) USING BTREE;
+
+--
+-- Indexes for table `fixed_ucharge`
+--
+ALTER TABLE `fixed_ucharge`
+  ADD PRIMARY KEY (`Unit_category`);
+
+--
+-- Indexes for table `suggestions_tou`
+--
+ALTER TABLE `suggestions_tou`
+  ADD PRIMARY KEY (`Suggest_id`,`Device_id`,`Bill_id`),
+  ADD KEY `FK10` (`Device_id`),
+  ADD KEY `FK12` (`Bill_id`),
+  ADD KEY `FK9` (`Cust_id`);
+
+--
+-- Indexes for table `tou_ucharge`
+--
+ALTER TABLE `tou_ucharge`
+  ADD PRIMARY KEY (`Time_category`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `Cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `Emp_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ebill_monthly_plan`
+--
+ALTER TABLE `ebill_monthly_plan`
+  ADD CONSTRAINT `FK1` FOREIGN KEY (`Cust_id`) REFERENCES `customer` (`Cust_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ebill_specialevent`
+--
+ALTER TABLE `ebill_specialevent`
+  ADD CONSTRAINT `FK2` FOREIGN KEY (`Cust_id`) REFERENCES `customer` (`Cust_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `electric_device_mplan`
+--
+ALTER TABLE `electric_device_mplan`
+  ADD CONSTRAINT `FK3` FOREIGN KEY (`Bill_id`) REFERENCES `ebill_specialevent` (`Bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK4` FOREIGN KEY (`Cust_id`) REFERENCES `customer` (`Cust_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK5` FOREIGN KEY (`Bill_id`) REFERENCES `ebill_monthly_plan` (`Bill_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `electric_device_special_event`
+--
+ALTER TABLE `electric_device_special_event`
+  ADD CONSTRAINT `FK6` FOREIGN KEY (`Bill_id`) REFERENCES `ebill_monthly_plan` (`Bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK7` FOREIGN KEY (`Bill_id`) REFERENCES `ebill_specialevent` (`Bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK8` FOREIGN KEY (`Cust_id`) REFERENCES `customer` (`Cust_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `suggestions_tou`
+--
+ALTER TABLE `suggestions_tou`
+  ADD CONSTRAINT `FK10` FOREIGN KEY (`Device_id`) REFERENCES `electric_device_mplan` (`Device_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK11` FOREIGN KEY (`Bill_id`) REFERENCES `ebill_monthly_plan` (`Bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK12` FOREIGN KEY (`Bill_id`) REFERENCES `ebill_specialevent` (`Bill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK9` FOREIGN KEY (`Cust_id`) REFERENCES `customer` (`Cust_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

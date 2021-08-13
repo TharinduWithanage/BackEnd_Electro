128a43e905d6390e38990e11a66fbecadb6beacf
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-08-13 10:16:02 +0530

    modified calculate bill

--- controller/monthlyBill/monthlyBillCalculate.js
+++ controller/monthlyBill/monthlyBillCalculate.js
@@ -1,9 +1,34 @@
 var commonResponseService = require('../../service/responseService');
 var addDeviceModel = require('../../model/monthlyBill/addDevicesModel');
 
+function CalculateUnits(power, minutes){
+
+    var numOfUnits = power * minutes * 60 / 3600000 ;
+    return numOfUnits;
+
+}
+
+function CalculateNumberOfMinutes(hors, minutes){
+
+    var numOfMinutes = (hors* 60 ) + minutes ;
+    return numOfMinutes;
+
+}
+
+function CalculateCost(uPrice, Units){
+
+    var cost = uPrice * Units;
+    return cost;
+
+}
+
 async function AddDeviceDataMain(request, response) {
 
     try {
+
+        var Device_details = Request.body ;
+
+        
 
         console.log("inside addDeviceDataMain Controller");
         // console.log(request.params.id);

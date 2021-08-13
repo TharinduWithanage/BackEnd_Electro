e4586b2ad371213c8551234de89dbaeae102cee3
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-08-13 13:29:59 +0530

    finalize backend part of add device bill

--- controller/monthlyBill/monthlyBillCalculate.js
+++ controller/monthlyBill/monthlyBillCalculate.js
@@ -1,5 +1,6 @@
 var commonResponseService = require('../../service/responseService');
 var addDeviceModel = require('../../model/monthlyBill/addDevicesModel');
+var unitChargesModel = require('../../model/cebengineer/unitChargesModel');
 
 function CalculateUnits(power, minutes) {
 
@@ -28,15 +29,27 @@ async function AddDeviceDataMain(request, response) {
 
         var Device_details = request.body;
 
+        var UnitPrice = await unitChargesModel.getUnitChargesDataFun("tou");
+        console.log(UnitPrice.data[0].Unit_charge);
+        console.log(UnitPrice.data[1].Unit_charge);
+        console.log(UnitPrice.data[2].Unit_charge);
+
+        var DayUnitCost = UnitPrice.data[0].Unit_charge;
+        var OffPeakUnitCost = UnitPrice.data[1].Unit_charge;
+        var PeakUnitCost = UnitPrice.data[2].Unit_charge;
+
+        
+
+
         Device_details.Using_minutes_peak_time = await CalculateNumberOfMinutes(Device_details.hPeak, Device_details.mPeak);
         Device_details.Using_minutes_off_peak_time = await CalculateNumberOfMinutes(Device_details.hOffPeak, Device_details.mOffPeak);
         Device_details.Using_minutes_day_time = await CalculateNumberOfMinutes(Device_details.hDay, Device_details.mDay);
         Device_details.Units_peak_time = await CalculateUnits(Device_details.power, Device_details.Using_minutes_peak_time);
         Device_details.Units_off_peak_time = await CalculateUnits(Device_details.power, Device_details.Using_minutes_off_peak_time);
         Device_details.Units_day_time = await CalculateUnits(Device_details.power, Device_details.Using_minutes_day_time);
-        Device_details.Cost_peak_time = await CalculateCost(54, Device_details.Units_peak_time);
-        Device_details.Cost_off_peak_time = await CalculateCost(13, Device_details.Units_off_peak_time);
-        Device_details.Cost_day_time = await CalculateCost(25, Device_details.Units_day_time);
+        Device_details.Cost_peak_time = await CalculateCost(PeakUnitCost, Device_details.Units_peak_time);
+        Device_details.Cost_off_peak_time = await CalculateCost(OffPeakUnitCost, Device_details.Units_off_peak_time);
+        Device_details.Cost_day_time = await CalculateCost(DayUnitCost, Device_details.Units_day_time);
         Device_details.Total_units_fixed = Device_details.Units_peak_time + Device_details.Units_off_peak_time + Device_details.Units_day_time;
         Device_details.Total_cost_TOU = Device_details.Cost_peak_time + Device_details.Cost_off_peak_time + Device_details.Cost_day_time;
 

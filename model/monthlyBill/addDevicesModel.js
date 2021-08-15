4c08458b3713a422a6cd2a0f0e696acaf8c16e6e
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-08-15 11:03:37 +0530

    finalize add device

--- model/monthlyBill/addDevicesModel.js
+++ model/monthlyBill/addDevicesModel.js
@@ -3,34 +3,38 @@ var db = require('../../database/databaseConnection');
 module.exports.AddDeviceMailBill = (devicedata) => {
     return new Promise(async (resolve, reject) => {
 
-        // console.log(requestData);
-        // console.log(devicedata);
-        var Bill_id = devicedata.Bill_id
-        var Device_name = devicedata.Device_name
-        var Quantity = devicedata.Quantity
-        var Priority = devicedata.Priority
-        var Using_minutes_peak_time = devicedata.Using_minutes_peak_time
-        var Using_minutes_off_peak_time = devicedata.Using_minutes_off_peak_time
-        var Using_minutes_day_time = devicedata.Using_minutes_day_time
-        var Power = devicedata.power
-        var Total_units_fixed = devicedata.Total_units_fixed
-        var Units_peak_time = devicedata.Units_peak_time
-        var Units_off_peak_time = devicedata.Units_off_peak_time
-        var Units_day_time = devicedata.Units_day_time
-        var Total_cost_TOU = devicedata.Total_cost_TOU
-        var Cost_peak_time = devicedata.Cost_peak_time
-        var Cost_off_peak_time = devicedata.Cost_off_peak_time
-        var Cost_day_time = devicedata.Cost_day_time
+        var bill_id = devicedata.bill_id
+        var appliance = devicedata.appliance
+        var quantity = devicedata.quantity
+        var priority = devicedata.priority
+        var using_minutes_peak_time = devicedata.using_minutes_peak_time
+        var using_minutes_off_peak_time = devicedata.using_minutes_off_peak_time
+        var using_minutes_day_time = devicedata.using_minutes_day_time
+        var power = devicedata.power
+        var total_units_fixed = devicedata.total_units_fixed
+        var units_peak_time = devicedata.units_peak_time
+        var units_off_peak_time = devicedata.units_off_peak_time
+        var units_day_time = devicedata.units_day_time
+        var total_cost_TOU = devicedata.total_cost_TOU
+        var cost_peak_time = devicedata.cost_peak_time
+        var cost_off_peak_time = devicedata.cost_off_peak_time
+        var cost_day_time = devicedata.cost_day_time
         var Cust_id = devicedata.Cust_id
+        var hPeak = devicedata.hPeak
+        var mPeak = devicedata.mPeak
+        var hOffPeak = devicedata.hOffPeak
+        var mOffPeak = devicedata.mOffPeak
+        var hDay = devicedata.hDay
+        var mDay = devicedata.mDay
 
 
         var addDeviceQuery = `INSERT INTO electric_device_mplan 
-        (Bill_id, Device_name, Quantity, Priority, Using_minutes_peak_time, Using_minutes_off_peak_time, Using_minutes_day_time, Power, Total_units_fixed, Units_peak_time,
-        Units_off_peak_time, Units_day_time, Total_cost_TOU, Cost_peak_time, Cost_off_peak_time, Cost_day_time, Cust_id) 
-        VALUES("${Bill_id}","${Device_name}","${Quantity}","${Priority}","${Using_minutes_peak_time}",
-        "${Using_minutes_off_peak_time}","${Using_minutes_day_time}","${Power}","${Total_units_fixed}","${Units_peak_time}",
-        "${Units_off_peak_time}","${Units_day_time}","${Total_cost_TOU}","${Cost_peak_time}","${Cost_off_peak_time}",
-        "${Cost_day_time}","${Cust_id}");`;
+        (bill_id, appliance, quantity, priority, hPeak, mPeak, hOffPeak, mOffPeak, hDay, mDay, using_minutes_peak_time, using_minutes_off_peak_time, using_minutes_day_time, power, total_units_fixed, units_peak_time,
+        units_off_peak_time, units_day_time, total_cost_TOU, cost_peak_time, cost_off_peak_time, cost_day_time, Cust_id) 
+        VALUES("${bill_id}","${appliance}","${quantity}","${priority}","${hPeak}","${mPeak}","${hOffPeak}","${mOffPeak}","${hDay}","${mDay}","${using_minutes_peak_time}",
+        "${using_minutes_off_peak_time}","${using_minutes_day_time}","${power}","${total_units_fixed}","${units_peak_time}",
+        "${units_off_peak_time}","${units_day_time}","${total_cost_TOU}","${cost_peak_time}","${cost_off_peak_time}",
+        "${cost_day_time}","${Cust_id}");`;
 
 
 

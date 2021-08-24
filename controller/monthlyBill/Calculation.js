7821f196b54a06f848ea25cb9db8ab812ca34b22
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-08-24 12:45:59 +0530

    modified the calculation function

--- controller/monthlyBill/Calculation.js
+++ controller/monthlyBill/Calculation.js
@@ -1,9 +1,24 @@
 var commonResponseService = require('../../service/responseService');
 
-function caculateFixedBill(noOfUnits , unitPrice){
+function caculateFixedBill(noOfUnits , unitPrice, fixedCharge){
+    var billValue ; 
     if(noOfUnits <= 30){
-        return noOfUnits * unitPrice;
-    }else{
-
+        billValue =  (noOfUnits * 2.50) + parseFloat(30) ;
+        return billValue;
+    }else if(noOfUnits <= 60){
+        billValue = parseFloat((noOfUnits - 60)* 4.85) + parseFloat(30* 2.50) + parseFloat(60);
+        return billValue;
+    }else if(noOfUnits <= 90){
+        billValue = parseFloat((noOfUnits - 60)* 10) + parseFloat(60* 7.85) + parseFloat(90);
+        return billValue;
+    }else if(noOfUnits <= 120){
+        billValue = parseFloat((noOfUnits - 90)* 27.75) + parseFloat(60* 7.85) + parseFloat(30* 10) + parseFloat(480);
+        return billValue;
+    }else if(noOfUnits <= 180){
+        billValue = parseFloat((noOfUnits - 120)* 32) + parseFloat(60* 7.85) + parseFloat(30* 10) + parseFloat(30* 27.75) + parseFloat(480);
+        return billValue;
+    }else if(noOfUnits > 180){
+        billValue = parseFloat((noOfUnits - 180)* 45) + parseFloat(60* 7.85) + parseFloat(30* 10) + parseFloat(30* 27.75) + parseFloat(60* 32) + parseFloat(540);
+        return billValue;
     }
 }

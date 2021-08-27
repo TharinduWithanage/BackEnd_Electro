e3ee348cd4f1385d630f3f11d3ede9263c958460
Minuri427 <yasarawickramanayaka@gmail.com>
2021-08-27 15:29:41 +0530

    ordered the device list

--- model/monthlyBill/deviceWiseModel.js
+++ model/monthlyBill/deviceWiseModel.js
@@ -6,7 +6,7 @@ module.exports.getDeviceUsageTou = (billId, userId) => {
 
 
 
-        var selectDeviceWiseTouQuery = `SELECT device_id,appliance, quantity,  cost_day_time, cost_off_peak_time, cost_peak_time ,total_units, total_cost_TOU FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId};`;
+        var selectDeviceWiseTouQuery = `SELECT device_id,appliance, quantity,  cost_day_time, cost_off_peak_time, cost_peak_time ,total_units, total_cost_TOU FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId} ORDER BY total_units DESC;`;
         console.log(selectDeviceWiseTouQuery)
 
         db.query(selectDeviceWiseTouQuery, async function (error, result) {
@@ -33,7 +33,7 @@ module.exports.getDeviceUsageFixed = (billId, userId) => {
 
 
 
-        var selectDeviceWiseFixedQuery = `SELECT appliance, quantity, total_units FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId};`;
+        var selectDeviceWiseFixedQuery = `SELECT appliance, quantity, total_units FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId} ORDER BY total_units DESC;`;
         console.log(selectDeviceWiseFixedQuery)
 
         db.query(selectDeviceWiseFixedQuery, async function (error, result) {

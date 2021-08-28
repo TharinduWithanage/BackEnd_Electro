28473575f7e0c7da715df013ad8d01113d998740
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-08-28 17:01:10 +0530

    add model for bill plans

---
+++ model/monthlyBill/billPlanModel.js
@@ -0,0 +1,87 @@
+var db = require('../../database/databaseConnection');
+
+
+module.exports.getMonthlyBillPlans = (CustId) => {
+
+    return new Promise(async (resolve, reject) => {
+
+
+        var selectQuery = `SELECT LEAST(Total_cost_tou,Total_cost_fixed) AS total_cost, bill_id, Best_model FROM ebill_monthly_plan WHERE Cust_id = ${CustId}; `;
+
+        //console.log("Inside get bill id model function query"+ selectQuery);
+
+
+        db.query(selectQuery, async function (error, result) {
+
+            if (error) {
+                console.log(error);
+
+                reject({ status: false, mesg: "error getting data" });
+            } else {
+
+                resolve({ status: true, data: result });
+
+            }
+
+        });
+    });
+
+}
+
+module.exports.deleteBillPlanFunc = (CustId, deleteData) => {
+    return new Promise(async (resolve, reject) => {
+        var bill_id = deleteData.bill_id;
+
+        console.log("Inside get deleteBillPlanFunc function query"+ bill_id);
+        console.log("Inside get deleteBillPlanFunc function query"+ CustId);
+
+        var deleteQuery = `DELETE FROM ebill_monthly_plan WHERE bill_id='${bill_id}' AND Cust_id='${CustId}';`;
+
+        console.log( deleteQuery);
+
+
+        db.query(deleteQuery, async function (error, result) {
+
+            if (error) {
+                console.log(error);
+
+                reject({ status: false, mesg: "error deleting data" });
+            } else {
+
+                resolve({ status: true, mesg: "Delete  device Success!!" });
+
+            }
+
+        });
+    });
+
+}
+
+module.exports.deleteBillPlanDevices = (CustId, deleteData) => {
+    return new Promise(async (resolve, reject) => {
+        var bill_id = deleteData.bill_id;
+
+        console.log("Inside get deleteBillPlanDevices function query"+ bill_id);
+        console.log("Inside get deleteBillPlanDevices function query"+ CustId);
+
+        var deleteQuery = `DELETE FROM electric_device_mplan WHERE bill_id='${bill_id}' AND Cust_id='${CustId}';`;
+
+        console.log( deleteQuery);
+
+
+        db.query(deleteQuery, async function (error, result) {
+
+            if (error) {
+                console.log(error);
+
+                reject({ status: false, mesg: "error deleting data" });
+            } else {
+
+                resolve({ status: true, mesg: "Delete  device Success!!" });
+
+            }
+
+        });
+    });
+
+}

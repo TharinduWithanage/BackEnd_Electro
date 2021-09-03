cb8823321b44a810bef869d284a704238c1ba5ec
hasinihatharasinghe97 <hasinividushanka@gmail.com>
2021-09-04 01:24:02 +0530

    Model Modified

--- model/dashboard/dashBoardModel.js
+++ model/dashboard/dashBoardModel.js
@@ -1,98 +1,113 @@
-var db = require('../../database/databaseConnection');
-
-
+var db = require("../../database/databaseConnection");
 
 /**
  * get count of unit charges update request and customers
  * @returns dashboard data
  */
 module.exports.empDashboardDataFun = () => {
-    return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    // console.log(requestData);
+    console.log("inside getReqCountFun");
 
-        // console.log(requestData);
-        console.log("inside getReqCountFun");
+    var selectQuery = `SELECT SUM(Update_ucharge_status + Update_fcharge_status) AS request_count FROM tou_ucharge;`;
+    var selectQuery1 = `SELECT SUM(Update_ucharge_status + Update_fcharge_status) AS request_count FROM fixed_ucharge;`;
+    var selectQuery2 = `SELECT COUNT(Cust_id) AS user_count FROM customer;`;
 
-        var selectQuery = `SELECT SUM(Update_ucharge_status + Update_fcharge_status) AS request_count FROM tou_ucharge;`;
-        var selectQuery1 = `SELECT SUM(Update_ucharge_status + Update_fcharge_status) AS request_count FROM fixed_ucharge;`;
-        var selectQuery2 = `SELECT COUNT(Cust_id) AS user_count FROM customer;`;
+    db.query(selectQuery, async function (error, result1) {
+      if (error) {
+        console.log(error);
 
-        db.query(selectQuery, async function (error, result1) {
+        reject({ status: false, mesg: "error getting data" });
+      } else {
+        db.query(selectQuery1, async function (error, result2) {
+          if (error) {
+            console.log(error);
 
-            if (error) {
+            reject({ status: false, mesg: "error getting data" });
+          } else {
+            db.query(selectQuery2, async function (error, result3) {
+              if (error) {
                 console.log(error);
 
                 reject({ status: false, mesg: "error getting data" });
-            } else {
-                db.query(selectQuery1, async function (error, result2) {
-
-                    if (error) {
-                        console.log(error);
-
-                        reject({ status: false, mesg: "error getting data" });
-                    } else {
-
-                        db.query(selectQuery2, async function (error, result3) {
-
-                            if (error) {
-                                console.log(error);
-
-                                reject({ status: false, mesg: "error getting data" });
-                            } else {
-                                var result = { result1, result2, result3 }
-                                resolve({ status: true, data: result });
-                            }
-
-                        });
-                    }
-
-                });
-
-
-            }
-
+              } else {
+                var result = { result1, result2, result3 };
+                resolve({ status: true, data: result });
+              }
+            });
+          }
         });
+      }
     });
-
-}
+  });
+};
 
 /**
- * 
+ *
  * @returns customer dashboard data
  */
 module.exports.custDashboardDataCountFun = (Cust_id) => {
-    return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    // console.log(requestData);
+    console.log("inside getReqCountFun");
 
-        // console.log(requestData);
-        console.log("inside getReqCountFun");
+    var selectQuery = `SELECT COUNT(bill_id) AS normalBill_count FROM ebill_monthly_plan WHERE Cust_id=${Cust_id};`;
+    var selectQuery1 = `SELECT COUNT(bill_id) AS specialBill_count FROM ebill_special_event_tou WHERE Cust_id=${Cust_id};`;
 
-        var selectQuery = `SELECT COUNT(bill_id) AS normalBill_count FROM ebill_monthly_plan WHERE Cust_id=${Cust_id};`;
-        var selectQuery1 = `SELECT COUNT(bill_id) AS specialBill_count FROM ebill_special_event_tou WHERE Cust_id=${Cust_id};`;
+    db.query(selectQuery, async function (error, result1) {
+      if (error) {
+        console.log(error);
 
-        db.query(selectQuery, async function (error, result1) {
+        reject({ status: false, mesg: "error getting data" });
+      } else {
+        db.query(selectQuery1, async function (error, result2) {
+          if (error) {
+            console.log(error);
 
-            if (error) {
-                console.log(error);
-
-                reject({ status: false, mesg: "error getting data" });
-            } else {
-                db.query(selectQuery1, async function (error, result2) {
-
-                    if (error) {
-                        console.log(error);
-
-                        reject({ status: false, mesg: "error getting data" });
-                    } else {
-
-                        var result = { result1, result2 }
-                        resolve({ status: true, data: result });
-                    }
-
-                });
-
-
-            }
-
+            reject({ status: false, mesg: "error getting data" });
+          } else {
+            var result = { result1, result2 };
+            resolve({ status: true, data: result });
+          }
         });
+      }
     });
+  });
+};
 
-}
+/**
+ * Get Pending Normal Unit Charges Function
+ * @param {*} id
+ * @returns
+ */
+module.exports.PendingNormalUnitChargesFun = (id) => {
+  return new Promise(async (resolve, reject) => {
+    // console.log(requestData);
+    console.log("inside PendingNormalUnitChargesFun");
+
+    if ((id = "unit")) {
+      var selectQuery = `SELECT Unit_category, Unit_charge, Update_unit_charges, Unit_charges_requested_date FROM fixed_ucharge WHERE Update_ucharge_status=1; `;
+    } else {
+      var selectQuery = `SELECT Unit_category, Fixed_charge, Update_fixed_charges, Fixed_charges_requested_date FROM fixed_ucharge WHERE Update_fcharge_status=1; `;
+    }
+
+    db.query(selectQuery, async function (error, result1) {
+      if (error) {
+        console.log(error);
+
+        reject({ status: false, mesg: "error getting data" });
+      } else {
+        db.query(selectQuery1, async function (error, result2) {
+          if (error) {
+            console.log(error);
+
+            reject({ status: false, mesg: "error getting data" });
+          } else {
+            var result = { result1, result2 };
+            resolve({ status: true, data: result });
+          }
+        });
+      }
+    });
+  });
+};

0d0afc59c98b0c8d23552e9a2d3ea3abf4f17f61
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-09-17 19:17:46 +0530

    modified

--- controller/monthlyBill/SuggestionsController.js
+++ controller/monthlyBill/SuggestionsController.js
@@ -1,5 +1,8 @@
 var commonResponseService = require('../../service/responseService');
 var SuggestionsModel = require('../../model/monthlyBill/suggestionModel');
+var addDeviceModel = require('../../model/monthlyBill/addDevicesModel');
+var monthlyBillCalculate = require('../monthlyBill/monthlyBillCalculate')
+var calculation = require('../monthlyBill/calculation')
 
 
 /**
@@ -32,4 +35,72 @@ async function GetDeviceWiseSuggestions(request, response) {
     }
 }
 
-module.exports = { GetDeviceWiseSuggestions };
+
+async function ApplySuggestions(request, response) { 
+
+    try {
+
+        console.log("inside ApplySuggestions Controller");
+
+        var CustId = request.params.id;
+
+        var suggestDetails = request.body.suggestDetails;
+
+        var DeviceData = await addDeviceModel.getDeviceDetailsMailBill(suggestDetails.bill_id, CustId, suggestDetails.device_id);
+
+        var DeviceDetails = DeviceData.data[0];
+
+
+        //await suggestionModel.deleteSuggestions(suggestDetails.device_id, CustId, suggestDetails.bill_id);
+       
+        console.log(DeviceDetails);
+        console.log(suggestDetails);
+
+        if(suggestDetails.cur_time == 'peak'){
+
+            if(suggestDetails.change_time == 'off peak'){
+                DeviceDetails.hPeak = DeviceDetails.hPeak - suggestDetails.can_change_hours;
+                DeviceDetails.mPeak = DeviceDetails.mPeak - suggestDetails.can_change_minutes;
+                DeviceDetails.hoffPeak = DeviceDetails.hoffPeak + suggestDetails.can_change_hours;
+                DeviceDetails.mOffPeak = DeviceDetails.mOffPeak + suggestDetails.can_change_minutes;
+
+            }else if(suggestDetails.change_time == 'day'){
+                DeviceDetails.hPeak = DeviceDetails.hPeak - suggestDetails.can_change_hours;
+                DeviceDetails.mPeak = DeviceDetails.mPeak - suggestDetails.can_change_minutes;
+                DeviceDetails.hDay = DeviceDetails.hDay + suggestDetails.can_change_hours;
+                DeviceDetails.mDay = DeviceDetails.mDay + suggestDetails.can_change_minutes;
+
+            }
+
+        }else if(suggestDetails.cur_time == 'day'){
+            if(suggestDetails.change_time == 'off peak'){
+                DeviceDetails.hDay = DeviceDetails.hDay - suggestDetails.can_change_hours;
+                DeviceDetails.mDay = DeviceDetails.mDay - suggestDetails.can_change_minutes;
+                DeviceDetails.hoffPeak = DeviceDetails.hoffPeak + suggestDetails.can_change_hours;
+                DeviceDetails.mOffPeak = DeviceDetails.mOffPeak + suggestDetails.can_change_minutes;
+            }
+        }
+
+        await monthlyBillCalculate.updateDeviceWithApplySugestion(DeviceDetails, CustId );
+
+        await calculation.calculatedBillValueUpdateAfterApplySuggest(suggestDetails.bill_id ,CustId);
+        
+        var Suggestions = await SuggestionsModel.getSuggestions(suggestDetails.bill_id , CustId);
+
+        if (Suggestions.data.length != 0) {
+            commonResponseService.responseWithData(response, Suggestions.data);
+
+        } else {
+
+            commonResponseService.errorWithMessage(response, "something went wrong");
+
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        commonResponseService.errorWithMessage(response, "something went wrong");
+    }
+}
+
+module.exports = { GetDeviceWiseSuggestions, ApplySuggestions };

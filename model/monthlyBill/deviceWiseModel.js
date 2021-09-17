d93299ef10ef2c6953d02674eb28b0951d728525
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-17 22:34:35 +0530

    query changed to get unit usages and cost usage tou

--- model/monthlyBill/deviceWiseModel.js
+++ model/monthlyBill/deviceWiseModel.js
@@ -6,7 +6,9 @@ module.exports.getDeviceUsageTou = (billId, userId) => {
 
 
 
-        var selectDeviceWiseTouQuery = `SELECT device_id,appliance, quantity,  cost_day_time, cost_off_peak_time, cost_peak_time ,total_units, total_cost_TOU FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId} ORDER BY total_units DESC;`;
+        // var selectDeviceWiseTouQuery = `SELECT device_id,appliance, quantity,  cost_day_time, cost_off_peak_time, cost_peak_time ,total_units, total_cost_TOU FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId} ORDER BY total_units DESC;`;
+        var selectDeviceWiseTouQuery = `SELECT * FROM electric_device_mplan WHERE Cust_id=${userId} AND bill_id=${billId} ORDER BY total_units DESC;`;
+
         console.log(selectDeviceWiseTouQuery)
 
         db.query(selectDeviceWiseTouQuery, async function (error, result) {
